e48e9f0e4853f7853b5a3c623cb2928c1851315d
Bryan's Air <battiste1313@gmail.com>
2018-10-25 15:12:35 -0600

    feat add link sign up button with Spotify signup

--- Newsic/LoginViewController.swift
+++ Newsic/LoginViewController.swift
@@ -10,13 +10,28 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    // MARK: Outlets
+    // MARK: Outlets and variables
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var signupButton: UIButton!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var debugTextLabel: UILabel!
     @IBOutlet weak var activityIndicatorLogin: UIActivityIndicatorView!
+    
+    // Lock phone orientation
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .portrait
+        } else {
+            return .portrait
+        }
+    }
+    
+    // Link sign up button with Spotify
+    @IBAction func signUp(_ sender: Any) {
+        UIApplication.shared.open(URL(string : "https://www.spotify.com/us/signup/")!, options: [:], completionHandler: { (status) in
+        })
+    }
     
     
     
