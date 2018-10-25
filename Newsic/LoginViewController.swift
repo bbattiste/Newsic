ceefcca3cd7bc91c5cb6babfe1250748f4b559aa
Bryan's Air <battiste1313@gmail.com>
2018-10-25 15:01:03 -0600

    feat add outlets to loginView/ add loginViewController file

---
+++ Newsic/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  Newsic
+//
+//  Created by Bryan's Air on 10/25/18.
+//  Copyright © 2018 Bryborg Inc. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: Outlets
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var debugTextLabel: UILabel!
+    @IBOutlet weak var activityIndicatorLogin: UIActivityIndicatorView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
