22d4990eb547a6f089f02ad941134daae5beca53
Bryan's Air <battiste1313@gmail.com>
2018-10-25 18:08:11 -0600

    test add code to get gesture recogniser to work

--- Newsic/ViewControllers/LoginViewController.swift
+++ Newsic/ViewControllers/LoginViewController.swift
@@ -59,20 +59,58 @@ class LoginViewController: UIViewController {
     // MARK: Functions
     
     private func completeLogin() {
-        
+        performUIUpdatesOnMain {
+            self.debugTextLabel.text = ""
+            self.setUIEnabled(true)
+            let controller = self.storyboard!.instantiateViewController(withIdentifier: "") as! UITabBarController
+            self.present(controller, animated: true, completion: nil)
+            self.loginButton.isEnabled = true
+            self.activityIndicatorLogin.stopAnimating()
+        }
     }
-
     
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func displayError(_ error: String){
+        performUIUpdatesOnMain {
+            self.setUIEnabled(true)
+            self.debugTextLabel.text = error
+        }
     }
-    */
+    
+}
 
+// MARK: - LoginViewController: UITextFieldDelegate
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    // textfields will return with enter key
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // textfields will return with touch on view
+    private func resignIfFirstResponder(_ textField: UITextField) {
+        if textField.isFirstResponder {
+            textField.resignFirstResponder()
+        }
+    }
+    
+    @IBAction func userDidtapView(_ sender: AnyObject) {
+        print("viewTapped")
+        resignIfFirstResponder(usernameTextField)
+        resignIfFirstResponder(passwordTextField)
+    }
+}
+
+// MARK: - LoginViewController (Configure UI)
+
+private extension LoginViewController {
+    
+    func setUIEnabled(_ enabled: Bool) {
+        usernameTextField.isEnabled = enabled
+        passwordTextField.isEnabled = enabled
+        loginButton.isEnabled = enabled
+        debugTextLabel.text = ""
+        debugTextLabel.isEnabled = enabled
+    }
 }
